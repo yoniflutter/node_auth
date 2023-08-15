7d4dc3aa674bea17533a0905c8fe2f326e4d89bf
yoniflutter <yohannessintayehu1@gmail.com>
2023-08-15 23:24:33 +0300

    Edited app.js file

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const mongoose = require('mongoose');
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static("./public"));
 
-mongoose.connect("")
+mongoose.connect("mongodb://127.0.0.1:27017/nodeauth")
 .then(()=>{
     console.log("Connected");
 }).catch((e)=>{
